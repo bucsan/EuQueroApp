08780c1766cfe21dda169e11216727b983ff63f4
IBPTE\thiago.bucsan <thiago.bucsan@ibpt.org.br>
2022-06-03 01:59:33 -0300

    Refatorando com a utilização do Dapper

--- EuQueroApp/src/Apresentacao/Funcionarios/FuncionarioGetAll.cs
+++ EuQueroApp/src/Apresentacao/Funcionarios/FuncionarioGetAll.cs
@@ -1,5 +1,5 @@
-﻿using System.Security.Claims;
-using Microsoft.AspNetCore.Identity;
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
 
 namespace EuQueroApp.Apresentacao.Funcionarios;
 
@@ -9,17 +9,21 @@ public class FuncionarioGetAll
     public static string[] Methods => new string[] { HttpMethod.Get.ToString() };
     public static Delegate Handle => Action;
 
-    public static IResult Action(int page, int rows, UserManager<IdentityUser> userManager)
+    public static IResult Action(int? page, int? rows, IConfiguration configuration)
     {
-        var users = userManager.Users.Skip((page - 1) * rows).Take(rows).ToList();
-        var funcionarios = new List<FuncionarioResponse>();
-        foreach (var item in users)
-        {
-            var claims = userManager.GetClaimsAsync(item).Result;
-            var claimName = claims.FirstOrDefault(c => c.Type == "Nome");
-            var userName =  claimName != null ? claimName.Value : string.Empty;
-            funcionarios.Add(new FuncionarioResponse(item.Email, userName));
-        }
+        var db = new SqlConnection(configuration["ConnectionString:EuQueroDb"]);
+        var query = @"SELECT 
+                        U.[Email], 
+                        C.[ClaimValue] AS Nome
+                    FROM
+                        AspNetUsers U
+                        INNER JOIN AspNetUserClaims C ON U.Id = C.UserId
+                                                      AND ClaimType = 'Nome'
+                    ORDER BY 
+                        Nome
+                    OFFSET(@page - 1) * @rows ROWS FETCH NEXT @rows ROWS ONLY";
+
+        var funcionarios = db.Query<FuncionarioResponse>(query, new {page, rows});
 
         return Results.Ok(funcionarios);
     }
