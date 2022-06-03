c04f44fcfefe9668bdfb430aeb5d33717d53ecd9
IBPTE\thiago.bucsan <thiago.bucsan@ibpt.org.br>
2022-06-03 02:10:50 -0300

    Refatorando para injeção de serviço

--- EuQueroApp/src/Apresentacao/Funcionarios/FuncionarioGetAll.cs
+++ EuQueroApp/src/Apresentacao/Funcionarios/FuncionarioGetAll.cs
@@ -1,5 +1,4 @@
-﻿using Dapper;
-using Microsoft.Data.SqlClient;
+﻿using EuQueroApp.Infraestrutura.Dados;
 
 namespace EuQueroApp.Apresentacao.Funcionarios;
 
@@ -9,22 +8,8 @@ public class FuncionarioGetAll
     public static string[] Methods => new string[] { HttpMethod.Get.ToString() };
     public static Delegate Handle => Action;
 
-    public static IResult Action(int? page, int? rows, IConfiguration configuration)
+    public static IResult Action(int? page, int? rows, QueryObterFuncionariosClaimNome query)
     {
-        var db = new SqlConnection(configuration["ConnectionString:EuQueroDb"]);
-        var query = @"SELECT 
-                        U.[Email], 
-                        C.[ClaimValue] AS Nome
-                    FROM
-                        AspNetUsers U
-                        INNER JOIN AspNetUserClaims C ON U.Id = C.UserId
-                                                      AND ClaimType = 'Nome'
-                    ORDER BY 
-                        Nome
-                    OFFSET(@page - 1) * @rows ROWS FETCH NEXT @rows ROWS ONLY";
-
-        var funcionarios = db.Query<FuncionarioResponse>(query, new {page, rows});
-
-        return Results.Ok(funcionarios);
+        return Results.Ok(query.Execute(page.Value, rows.Value));
     }
 }
---
+++ EuQueroApp/src/Infraestrutura/Dados/QueryObterFuncionariosClaimNome.cs
@@ -0,0 +1,32 @@
+﻿using Dapper;
+using EuQueroApp.Apresentacao.Funcionarios;
+using Microsoft.Data.SqlClient;
+
+namespace EuQueroApp.Infraestrutura.Dados;
+
+public class QueryObterFuncionariosClaimNome
+{
+    private readonly IConfiguration configuration;
+
+    public QueryObterFuncionariosClaimNome(IConfiguration configuration)
+    {
+        this.configuration = configuration;
+    }
+
+    public IEnumerable<FuncionarioResponse> Execute(int page, int rows)
+    {
+        var db = new SqlConnection(configuration["ConnectionString:EuQueroDb"]);
+        var query = @"SELECT 
+                        U.[Email], 
+                        C.[ClaimValue] AS Nome
+                    FROM
+                        AspNetUsers U
+                        INNER JOIN AspNetUserClaims C ON U.Id = C.UserId
+                                                      AND ClaimType = 'Nome'
+                    ORDER BY 
+                        Nome
+                    OFFSET(@page - 1) * @rows ROWS FETCH NEXT @rows ROWS ONLY";
+
+        return db.Query<FuncionarioResponse>(query, new { page, rows });
+    }
+}
